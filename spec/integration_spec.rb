5038e5d8f9ead92776f701e244482df4a0bcd5c2
Geoff Williams <geoff@geoffwilliams.me.uk>
2017-04-11 09:52:11 +1000

    dont do vagrant tests on travis

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,16 +4,22 @@ require 'puppetbox/driver/vagrant'
 RSpec.describe PuppetBox do
   CODE_FIXTURE = File.join(File.dirname(File.expand_path(__FILE__)), "../spec/fixtures/production")
 
-  it "reports passing code correctly" do
-    di = PuppetBox::Driver::Vagrant.new('test',CODE_FIXTURE)
-    res = PuppetBox.run_puppet(di, "passing")
-    expect(res.passed).to be true
-  end
+  # Cannot run vagrant in travis ;-)
+  # https://github.com/travis-ci/travis-ci/issues/6060
+  #
+  # ...damn!  That means we must detect CI system and not run tests there.  It's
+  # therefore vital that `bundle exec rake spec` is run manually for each commit
+  if ENV['CI'] != 'true'
+    it "reports passing code correctly" do
+      di = PuppetBox::Driver::Vagrant.new('test',CODE_FIXTURE)
+      res = PuppetBox.run_puppet(di, "passing")
+      expect(res.passed).to be true
+    end
 
-  it "reports failing code correctly" do
-    di = PuppetBox::Driver::Vagrant.new('test',CODE_FIXTURE)
-    res = PuppetBox.run_puppet(di, "failing")
-    expect(res.passed).to be false
+    it "reports failing code correctly" do
+      di = PuppetBox::Driver::Vagrant.new('test',CODE_FIXTURE)
+      res = PuppetBox.run_puppet(di, "failing")
+      expect(res.passed).to be false
+    end
   end
-
 end
